e774f8ef0bcab78df37e9efe7c751e6c9ff2d4aa
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-29 12:14:34 +0000

    jquery added to all buttons

---
+++ src/interface.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+  var thermostat = new Thermostat;
+  $('#temperature').text(updateTemperature());
+
+  $('#increase-temp').on('click', function() {
+    thermostat.inscreaseTemperature();
+    updateTemperature();
+  });
+
+  $('#decrease-temp').on('click', function() {
+    thermostat.decreaseTemperature();
+    updateTemperature();
+  });
+
+  $('#reset-temp').on('click', function() {
+    thermostat.resetTemperature();
+    updateTemperature();
+  });
+
+  $('#psm-on').on('click', function() {
+    thermostat.powerSavingOn();
+    $('#psm-status').text('On');
+    updateTemperature();
+  });
+
+  $('#psm-off').on('click', function() {
+    thermostat.powerSavingOff();
+    updateTemperature();
+  });
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.getCurrentTemperature());
+    updatePSMStatus();
+  };
+
+  function updatePSMStatus() {
+    if (thermostat.isPowerSaving) {
+      $('#psm-status').text('On');
+    } else {
+      $('#psm-status').text('Off');
+    }
+  };
+
+});
