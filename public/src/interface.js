99412e3658516ff0ea31db40482b22cb2d9eeeb2
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-30 13:23:53 +0000

    temp saved on refresh

--- public/src/interface.js
+++ public/src/interface.js
@@ -63,7 +63,7 @@ $(document).ready(function() {
 
   function displayWeather(city) {
     var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
-    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var token = 'API KEY';
     var units = '&units=metric';
     $.get(url + token + units, function(data) {
       $('#current-temperature').text(data.main.temp)
